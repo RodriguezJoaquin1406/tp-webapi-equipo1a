363ea866e62cc235bb787f044daba066d2cdf9fe
Joaquin Rodriguez <ertsyvler@gmail.com>
2025-10-08 23:18:49 -0300

    primer paso articulos controller

---
+++ TPWebApi_Equipo1A/Controllers/ArticuloController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TPWebApi_Equipo1A.Models;
+using dominio;
+using negocio;
+
+namespace TPWebApi_Equipo1A.Controllers
+{
+    public class ArticuloController : ApiController
+    {
+        // GET: api/Articulo
+        public IEnumerable<Articulo> Get()
+        {
+            try
+            {
+                articuloNegocio negocio = new articuloNegocio();
+                return negocio.listar();
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+
+        // GET: api/Articulo/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Articulo
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Articulo/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Articulo/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TPWebApi_Equipo1A/Models/ArticuloDto.cs
@@ -0,0 +1,18 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPWebApi_Equipo1A.Models
+{
+    public class ArticuloDto
+    {
+        public string CodigoArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int IdMarca { get; set; }
+        public int IdCategoria { get; set; }
+    }
+}
